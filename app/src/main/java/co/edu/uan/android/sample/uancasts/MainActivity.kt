90b63a6a32bfa0c30a0616261ec5ce29b394528c
wjfatuan <wilson.forero@uan.edu.co>
2022-07-21 18:30:36 -0500

    Adding view binding

--- app/src/main/java/co/edu/uan/android/sample/uancasts/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/sample/uancasts/MainActivity.kt
@@ -2,10 +2,15 @@ package co.edu.uan.android.sample.uancasts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.edu.uan.android.sample.uancasts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
